093245617f6c1a7b1c662a2c72fccec418162a8f
Lori Escobar <lorimcescobar@hotmail.com>
2018-06-06 16:19:20 -0400

    2nd Ruby server with README.TXT file attached

--- server.rb
+++ server.rb
@@ -1,14 +1,19 @@
 require "sinatra"
 
-get '/' do 
-   "Hello world"
+get 'http://localhost:4567/' do
+  "test"
 end
 
 # Above is defining the endpoint - by using '/' then this is the root.
 
-get '/sinatra' do
-	"Hey Sinatra.  Sing it to me."
-end
+# get '/sinatra' do
+# 	"Hey Sinatra.  Sing it to me."
+# end
 
-# new second endpoint
+# # new second endpoint
 
+# require 'sinatra'
+
+# get '/' do
+# 	"test"
+# end
